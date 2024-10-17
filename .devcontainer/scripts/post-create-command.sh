6eb375b299f4886af047cc1dc8ab89cabe1ba0e5
Kohsuk <kohsuk.tech@gmail.com>
2024-10-17 00:03:29 +0000

    chore: edited post-create-command.sh and code-workspace

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -4,6 +4,3 @@
 bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -b . ory
 
 sudo mv ./ory /usr/local/bin/
-
-# Ensure the /usr/local/bin directory is in the PATH
-export PATH=$PATH:/usr/local/bin
