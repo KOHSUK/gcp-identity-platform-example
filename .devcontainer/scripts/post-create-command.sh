862d2c11df40fa439b8ed32baecd77523ba4bbdf
Kohsuk <kohsuk.tech@gmail.com>
2024-10-21 15:31:16 +0000

    chore: update ORY Hydra installation script

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Download and run the ORY installation script
-bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -b . ory
+bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -d -b . hydra v2.2.0
 
-sudo mv ./ory /usr/local/bin/
+sudo mv ./hydra /usr/local/bin/
